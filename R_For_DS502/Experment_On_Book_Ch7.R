3d5b342ae7dfbdfcd6c4b6cc5ec1844429181e15
mzlcuhk <mzlcuhk@hotmail.com>
2016-11-06 17:25:03 -0400

    HW4

--- R_For_DS502/Experment_On_Book_Ch7.R
+++ R_For_DS502/Experment_On_Book_Ch7.R
@@ -51,3 +51,93 @@ plot(age,I(wage>250),xlim=agelims,type="n",ylim=c(0,.2))
 points(jitter(age),I((wage>250)/5),cex=.5,pch="|",col="darkgrey")
 lines(age.grid,pfit,lwd=2,col="blue")
 matlines(age.grid,se.bands,lwd=1,col="blue",lty=3)
+
+
+table(cut(age,4))
+fit=lm(wage~cut(age,4),data=Wage)
+# cut() will return a categarial var.
+coef(summary(fit))
+preds=predict(fit,newdata=list(age=age.grid),se=T)
+se.bands=cbind(preds$fit+2*preds$se.fit,preds$fit-2*preds$se.fit)
+plot(age,wage,xlim=agelims,cex=.5,col='darkgrey')
+title("Degree-4 Polynomial",outer = T)
+lines(age.grid,preds$fit,lwd=2,col="blue")
+matlines(age.grid,se.bands,lwd=1,col="blue",lty=3)
+
+#spline
+library(splines)
+fit=lm(wage~bs(age,knots=c(25,40,60)),data=Wage)
+# bs is basis spline
+# arguement degree can be used here
+pred=predict(fit,newdata=list(age=age.grid),se=T)
+plot(age,wage,col="gray")
+lines(age.grid,pred$fit,lwd=2)
+lines(age.grid,pred$fit+pred$se.fit*2,lty="dashed")
+lines(age.grid,pred$fit-pred$se.fit*2,lty="dashed")
+dim(bs(age,knots=c(25,40,60)))
+dim(bs(age,df=6))
+# df is degree of freedom
+attr(bs(age,df=6),"knots")
+
+# natural spline
+fit2=lm(wage=ns(age,4),data=Wage)
+#ns() is natural spline
+pred2=predict(fit2,newdata=list(age=age.grid),se=T)
+lines(age.grid,pred2$fit,col="red",lwd=2)
+
+# smooth spline
+plot(age,wage,xlim=agelims,cex=0.5,col="darkgrey")
+title("Smooth spline")
+fit=smooth.spline(age,wage,df=16)
+fit2=smooth.spline(age,wage,cv=T)
+fit2$df
+lines(fit,col="red",lwd=2)
+lines(fit2,col="blue",lwd=2)
+legend("topright",legend=c("16 DF","6.8 DF"),col=c("red","blue"),lty=1,lwd=2,cex=.8)
+
+#local regression
+par(mfrow=c(1,1))
+plot(age,wage,xlim=agelims,cex=0.5,col="darkgrey")
+title("Local regression")
+fit=loess(wage~age,span=.2,data=Wage)
+fit2=loess(wage~age,span=.5,data=Wage)
+lines(age.grid,predict(fit,data.frame(age=age.grid),col="red",lwd=2))
+lines(age.grid,predict(fit2,data.frame(age=age.grid),col="blue",lwd=2))
+legend("topright",legend=c("Span=0.2","Span=0.5"),col=c("red","blue"),lty=1,lwd=2,cex=.8)
+
+#GAM
+gam1=lm(wage~ns(year,4)+ns(age,5)+education,data=Wage)
+library(gam)
+gam.m1=gam(wage~s(age,5)+education,data=Wage)
+# s for spline
+gam.m2=gam(wage~year+s(age,5)+education,data=Wage)
+gam.m3=gam(wage~s(year,4)+s(age,5)+education,data=Wage)
+par(mfrow=c(1,3))
+plot(gam.m3,se=T,col="blue")
+plot.gam(gam1,se=T,col="red")
+anova(gam.m1,gam.m2,gam.m3,test="F")
+summary(gam.m3)
+preds=predict(gam.m2,newdata=Wage)
+
+gam.lo=gam(wage~s(year,df=4)+lo(age,span=0.7)+education,data=Wage)
+# lo for local regression
+plot(gam.lo,se=T,col="green")
+
+
+gam.lo.i=gam(wage~lo(year,age,span=0.5)+education,data=Wage)
+# Interaction
+library(akima)
+plot(gam.lo.i)
+
+# GAM for Logistic
+gam.lr=gam(I(wage>250)~year+s(age,df=5)+education,family=binomial,data=Wage)
+par(mfrow=c(1,3))
+plot(gam.lr,se=T,col="green")
+table(education,I(wage>250))
+
+# remove HS group from model
+gam.lr.s=gam(I(wage>250)~year+s(age,df=5)+education,family=binomial,data=Wage,
+             subset=(education!="1. < HS Grad"))
+par(mfrow=c(1,3))
+plot(gam.lr.s,se=T,col="green")
+             
